cfc52c29edb4d50cde35a2e5da77ffc5eb3be237
vignesh guntapally <vigneshraoedu@gmail.com>
2026-07-30 10:43:49 +0100

    feat: validate products and apply basket offers

--- server/src/domain/BasketPricer.js
+++ server/src/domain/BasketPricer.js
@@ -1,18 +1,32 @@
 import { penceToPounds } from "./money.js";
 
 export class BasketPricer {
-  price(basket, catalogue) {
+  price(basket, catalogue, offers) {
     const items = basket.items;
     let subTotalPence = 0;
 
     for (const [product, quantity] of Object.entries(items)) {
+      if (!catalogue.hasProduct(product)) {
+        throw new Error(
+          `Product '${product}' is in the basket but not in the catalogue`
+        );
+      }
+
       subTotalPence += catalogue.getPricePence(product) * quantity;
     }
 
+    const discountPence = offers.reduce(
+      (sum, offer) =>
+        sum + offer.calculateDiscountPence(items, catalogue),
+      0
+    );
+
+    const totalPence = subTotalPence - discountPence;
+
     return {
       subTotal: penceToPounds(subTotalPence),
-      discount: 0,
-      total: penceToPounds(subTotalPence)
+      discount: penceToPounds(discountPence),
+      total: penceToPounds(totalPence)
     };
   }
 }
